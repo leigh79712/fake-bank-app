a534fff33dc783402ee12a60a8ccd9b3214f7c79
蔡旻勲 <hellohayeah@gmail.com>
2021-12-28 16:02:19 +0800

    feat: add footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,67 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/images/icon.png";
+
+const Links = [
+  { name: "About", url: "/about" },
+  { name: "Pricing", url: "/pricing" },
+  { name: "Terms of Use", url: "/terms-of-use" },
+  { name: "Privacy Policy", url: "/privacy-policy" },
+  { name: "Careers", url: "/careers" },
+  { name: "Blog", url: "/blog" },
+  { name: "Contact Us", url: "/contact-us" },
+];
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer
+      css={css`
+        padding: 10rem 3rem;
+        background-color: #37383d;
+        text-align: center;
+      `}
+    >
+      <ul
+        css={css`
+          display: flex;
+          justify-content: center;
+          list-style: none;
+          grid-gap: 4rem;
+          margin-bottom: 5rem;
+        `}
+      >
+        {Links.map((link) => (
+          <li>
+            <Link href={link.url} key={link.name}>
+              <a
+                css={css`
+                  font-size: 1.6rem;
+                  color: #eee;
+                  text-decoration: none;
+                  cursor: pointer;
+                `}
+              >
+                {link.name}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Image src={logo} alt="logo" width={50} height={50} />
+      <p
+        css={css`
+          font-size: 1.4rem;
+          color: #aaa;
+          text-align: center;
+          margin-top: 5rem;
+        `}
+      >
+        © Copyright by Jonas Schmedtmann. Use for learning or your portfolio.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
