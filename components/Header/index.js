5134f42b352296151b7730926ce417aa176442e0
蔡旻勲 <hellohayeah@gmail.com>
2021-12-28 14:32:30 +0800

    refactor: Initialize Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,26 +1,5 @@
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import Link from "next/link";
-
 const Header = () => {
-  return (
-    <header>
-      <nav>
-        <Link href="/">
-          <a
-            css={css`
-              width: 150px;
-              height: 45px;
-              display: inline-block;
-              background-image: url("/images/logo.png");
-              background-size: contain;
-              background-repeat: no-repeat;
-            `}
-          />
-        </Link>
-      </nav>
-    </header>
-  );
+  return <header>header</header>;
 };
 
 export default Header;
