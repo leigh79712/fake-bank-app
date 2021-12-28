02d48b095f93be4fbe43872fbdb2bfaf0aed8c95
Leigh <leigh79712@hotmail.com>
2021-12-28 23:14:46 +0100

    feat: header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,82 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/images/logo.png";
+
+const Links = [
+  { name: "Features", url: "/features" },
+  { name: "Operations", url: "/operations" },
+  { name: "Testimonials", url: "/testimonials" },
+];
+
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header
+      css={css`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 9rem;
+        padding: 0 6rem;
+        z-index: 100;
+      `}
+    >
+      <a>
+        <Image src={logo} alt="logo" height={50} width={160} />
+      </a>
+      <ul
+        css={css`
+          display: flex;
+          align-items: center;
+          list-style: none;
+        `}
+      >
+        {Links.map((link) => (
+          <li
+            key={link.name}
+            css={css`
+              margin-left: 4rem;
+            `}
+          >
+            <Link href={link.name}>
+              <a
+                css={css`
+                  font-size: 1.7rem;
+                  font-weight: 400;
+                  color: inherit;
+                  text-decoration: none;
+                  display: block;
+                `}
+              >
+                {link.name}
+              </a>
+            </Link>
+          </li>
+        ))}
+        <li
+          css={css`
+            margin-left: 4rem;
+          `}
+        >
+          <Link href="/open-account">
+            <a
+              css={css`
+                font-size: 1.7rem;
+                font-weight: 400;
+                color: #222;
+                background-color: #5ec576;
+                padding: 0.8rem 2.5rem;
+                border-radius: 3rem;
+              `}
+            >
+              Open account
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
