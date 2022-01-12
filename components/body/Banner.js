6c8772109d2c9cbdc8455879bf72800cd4231358
Leigh <leigh79712@hotmail.com>
2022-01-12 13:28:32 +0100

    banner

--- components/body/Banner.js
+++ components/body/Banner.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { css } from "@emotion/react";
 import Button from "./Button";
-
+import card from "../../public/images/card.jpeg";
 const Banner = () => {
   return (
     <div
@@ -32,11 +32,24 @@ const Banner = () => {
             css={css`
               font-size: 5.5rem;
               line-height: 1.35;
+              color: #444;
             `}
           >
             When banking meets minimalist
           </h1>
+          <h4
+            css={css`
+              font-size: 2.4rem;
+              font-weight: 500;
+              color: #444;
+            `}
+          >
+            Make your life easier
+          </h4>
           <Button>Register</Button>
+        </div>
+        <div>
+          <Image src={card} alt="card"></Image>
         </div>
       </div>
     </div>
