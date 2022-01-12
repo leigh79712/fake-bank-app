3bbe99cfac5b2127017f64a32cae5692f14f49dd
Leigh <leigh79712@hotmail.com>
2022-01-12 15:19:26 +0100

    feature section title

---
+++ components/body/Feature.js
@@ -0,0 +1,15 @@
+/** @jsxImportSource @emotion/react */
+import React, { useState } from "react";
+import { css } from "@emotion/react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Feature = () => {
+  return (
+    <div>
+      <h2>FEATURE</h2>
+    </div>
+  );
+};
+
+export default Feature;
