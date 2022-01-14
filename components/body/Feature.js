72fefe9497367d1d2c58dce7db96ece007eced32
Leigh <leigh79712@hotmail.com>
2022-01-14 18:57:58 +0100

    feature/grid layout adding pics

--- components/body/Feature.js
+++ components/body/Feature.js
@@ -3,11 +3,96 @@ import React, { useState } from "react";
 import { css } from "@emotion/react";
 import Link from "next/link";
 import Image from "next/image";
+import SectionTitle from "./SectionTitle";
+import SectionHeader from "./SectionHeader";
+import bank from "../../public/images/bank.jpeg";
+import saving from "../../public/images/saving.jpeg";
+import piggybank from "../../public/images/piggybank.jpeg";
 
 const Feature = () => {
   return (
-    <div>
-      <h2>FEATURE</h2>
+    <div
+      css={css`
+        padding: 5em 13em;
+      `}
+    >
+      <SectionTitle>FEATURE</SectionTitle>
+      <SectionHeader>
+        Everything you need in a modern bank and More
+      </SectionHeader>
+      <div
+        css={css`
+          margin: 4em auto;
+          display: grid;
+          grid-template-columns: 4fr 4fr;
+          grid-gap: 3em;
+        `}
+      >
+        <div>
+          <Image src={bank} height={550} width={880}></Image>
+        </div>
+        <div
+          css={css`
+            padding: 5em;
+          `}
+        >
+          <h5
+            css={css`
+              font-size: 2rem;
+              margin-bottom: 1rem;
+            `}
+          >
+            100% digital bank
+          </h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            aperiam vero sed corporis voluptatem voluptatum nulla?
+          </p>
+        </div>
+
+        <div
+          css={css`
+            padding: 5em;
+          `}
+        >
+          <h5
+            css={css`
+              font-size: 2rem;
+              margin-bottom: 1rem;
+            `}
+          >
+            Watch your money grow
+          </h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            aperiam vero sed corporis voluptatem voluptatum nulla?
+          </p>
+        </div>
+        <div>
+          <Image src={saving} height={550} width={880}></Image>
+        </div>
+        <div>
+          <Image src={piggybank} height={550} width={880}></Image>
+        </div>
+        <div
+          css={css`
+            padding: 5em;
+          `}
+        >
+          <h5
+            css={css`
+              font-size: 2rem;
+              margin-bottom: 1rem;
+            `}
+          >
+            Free debit card included
+          </h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            aperiam vero sed corporis voluptatem voluptatum nulla?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Banner from "../components/body/Banner";
+import Feature from "../components/body/Feature";
 import Layout from "../components/Layout";
 
 const Index = () => {
   return (
     <Layout>
       <Banner></Banner>
+      <Feature></Feature>
     </Layout>
   );
 };
