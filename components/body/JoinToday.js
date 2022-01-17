c5de2881d783c27b28fdc33a5d53c103e7ce69f9
Leigh <leigh79712@hotmail.com>
2022-01-17 20:35:30 +0100

    Jointoday/banner

--- components/body/JoinToday.js
+++ components/body/JoinToday.js
@@ -1,12 +1,32 @@
 /** @jsxImportSource @emotion/react */
 import React, { useState } from "react";
 import { css } from "@emotion/react";
-
+import Button from "./Button";
+import SectionHeader from "./SectionHeader";
 const JoinToday = () => {
   return (
-    <>
-      <Button>Register</Button>
-    </>
+    <div
+      css={css`
+        margin: 4em 0;
+      `}
+    >
+      <SectionHeader>Do your banking where and when you like</SectionHeader>
+      <div
+        css={css`
+          text-align: center;
+        `}
+      >
+        <Button
+          css={css`
+            margin: 2em;
+            font-size: 1.5em;
+            border-radius: 50px;
+          `}
+        >
+          Join Us Today
+        </Button>
+      </div>
+    </div>
   );
 };
 
