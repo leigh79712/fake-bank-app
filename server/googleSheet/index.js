f039443f7493247aab31d9223bdb8fdf5fd9b5b2
蔡旻勲 <hellohayeah@gmail.com>
2022-01-19 22:42:02 +0800

    feat: connect google sheets API

---
+++ server/googleSheet/index.js
@@ -0,0 +1,18 @@
+const { google } = require("googleapis");
+
+module.exports = async (range) => {
+  const auth = new google.auth.GoogleAuth({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+  });
+  const sheets = google.sheets({ version: "v4", auth });
+
+  try {
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId: "1-i7G11p1VQZDeEEsVDelelFsP-IZ4nIEIyNne0TRLPo",
+      range,
+    });
+
+    const { data } = res;
+    console.log(data);
+  } catch (error) {}
+};
