0e6f724dde0a9807cadc3e6e45b92d11e79f70ab
Leigh <leigh79712@hotmail.com>
2022-02-11 22:27:25 +0800

    koa-passport

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
+  movements: Array,
+  movementsDate: Array,
 });
 
 module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 const Koa = require("koa");
 const next = require("next");
 const Router = require("@koa/router");
+const json = require("koa-json");
+const bodyParser = require("koa-bodyparser");
+const passport = require("koa-passport");
+const session = require("koa-session");
 
 const port = 3000;
 const dev = "development";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+// const User = require("../models/user");
 const mongoose = require("mongoose");
+const { authenticate } = require("passport");
 // const MongoStore = require("connect-mongo");
 const mongo_Url =
   "mongodb+srv://dbUser:W7aMnwMsSSGDcBBF@cluster0.6epjm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -19,10 +25,31 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+// const authenticate = (username, password) => {};
 
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
+
+  server.keys = ["your-session-secret"];
+  server.use(session({}, server));
+
+  server.use(bodyParser());
+  server.use(json());
+
+  // require("../lib/auth");
+  server.use(passport.initialize());
+  server.use(passport.session());
+
+  // passport.use(new LocalStrategy(User.authenticate()));
+  // passport.serializeUser(User.serializeUser());
+  // passport.deserializeUser(User.deserializeUser());
+
+  router.post("/login", (ctx) => {
+    // const { username, password } = ctx.body;
+    const { username, password } = ctx.request.body;
+    authenticate(username, password);
+  });
 
   router.all("(.*)", async (ctx) => {
     await handle(ctx.req, ctx.res);
