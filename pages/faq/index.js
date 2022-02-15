2884f0ad42dac66a1179852d702c53eee4890808
蔡旻勲 <hellohayeah@gmail.com>
2022-02-15 22:21:16 +0800

    fix: fix building project

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,16 +1,15 @@
 const Faq = (data) => {
-  console.log(data);
   return <main>faq</main>;
 };
 
 // server side fetch data
 
-import getGoogleSheet from "server/googleSheet";
+// import getGoogleSheet from "server/googleSheet";
 
-export const getStaticProps = async () => {
-  const data = await getGoogleSheet("最新情報!A1:C");
+// export const getStaticProps = async () => {
+//   const data = await getGoogleSheet("最新情報!A1:C");
 
-  return { props: data };
-};
+//   return { props: data };
+// };
 
 export default Faq;
