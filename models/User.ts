4566b8b19ce0f90d1a054d8b5d59c5b9c1869798
Jerry Tsai <jerry_tsai@taogo.com.tw>
2022-11-16 16:14:57 +0800

    chore: update files

--- models/User.ts
+++ models/User.ts
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 // const passportLocalMongoose = require("passport-local-mongoose");
 
 const UserSchema = new mongoose.Schema({
+  id: { type: Number, require: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   firstname: String,
   lastname: String,
-  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   movements: Array,
 });
 
 // UserSchema.plugin(passportLocalMongoose);
-
 export default mongoose.models.User || mongoose.model("User", UserSchema);
