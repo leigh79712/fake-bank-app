2096bcd0cc62b7cea78b18e875966b44cc491f93
Jerry Tsai <jerry_tsai@taogo.com.tw>
2022-12-21 16:22:46 +0800

    refactor: remove old components

--- components/common/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Layout } from "./Layout";
-export { default as Button } from "./Button";
-export { default as Input } from "./Input";
-export { default as Label } from "./Label";
-export { default as SectionHeader } from "./SectionHeader";
-export { default as SectionTitle } from "./SectionTitle";
